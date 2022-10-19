503cbaf4f8412e070849aa5d13e530014adb516d
Alex Wang <wangxi@saskpolytech.ca>
2022-10-19 11:33:19 -0600

    Start Mat Trans

--- code/Matrix/src/main/java/IMatrix.java
+++ code/Matrix/src/main/java/IMatrix.java
@@ -3,4 +3,9 @@ public interface IMatrix {
     IMatrix Multiply(IMatrix mRight)throws IllegalArgumentException ;
     IMatrix ScalarMultiply(double dScalar);
     IMatrix Subtract(IMatrix mRight)throws IllegalArgumentException ;
+
+    IMatrix Scaling(double dX, double dY);
+    IMatrix Translate(double dX, double dY);
+    IMatrix Rotate(double radians);
+    IMatrix Rotate(double radians, double dOx, double dOy);
 }
